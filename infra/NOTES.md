063c7d552a51d4c48772b6dace37d714e98c9e38
jom-hedges <jom_hedges@proton.me>
2025-10-26 21:29:14 +0800

    updated notes, created vpc, routing table, and gateway for tofu-starter module

---
+++ infra/NOTES.md
@@ -0,0 +1,25 @@
+## 2025-10-25
+
+Current problem: provisioning a Linux instance on AWS that I can SSH into, and run Bun.
+
+`ping: ami-0244ef75e95122fd9@ec2-18-180-150-130.ap-northeast-1.compute.amazonaws.com: Name or service not known`
+
+The service's public IP address is unknown, so in the `aws_instance` I added the argument `associate_public_ip_address`, and after running `tofu apply`,
+the error persisted. 
+
+Yesterday, I ran a command to check that this ec2 instance's `id` and `KeyName` corresponded, and the table returned `NONE` in the `keyName` column. That led me
+to add an `aws_key_pair` resource, but when I ran `tofu apply`, the program warned that
+
+```
+importing EC2 Key Pair (greg-rabbit-key): operation error EC2: ImportKeyPair, ... api error InvalidKeyPair.Duplicate: The keypair already exists
+```
+So I am working this issue this evening. Listening to Weyes Blood's album ["And in the Darkness, Hearts Aglow"](https://weyesblood.bandcamp.com/album/and-in-the-darkness-hearts-aglow).
+
+Thinking about this message, `Name or service not known` refers to an unknown IP address, which I think might come because of a missing `aws_vpc` resource.
+
+## 2025-10-26
+Evening time, created an `aws_subnet` resourced called `rabbit-vpc-public`, which I thought would add a gateway to assist in discovering the network. 
+
+This very old [stackoverflow post](https://stackoverflow.com/questions/2813843/possible-reasons-for-timeout-when-trying-to-access-ec2-instance#15004658) pointed me in the correct direction to ensure that I had a vpc, internet gateway, routing table, and associations created.
+
+
--- infra/tofu-starter/main.tf
+++ infra/tofu-starter/main.tf
@@ -10,10 +10,48 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-2"
+    region = "ap-northeast-1"
 }
 
-data "aws_ami" "ubuntu" {
+resource "aws_vpc" "rabbit-vpc-public" {
+    cidr_block              = "10.0.0.0/16"
+    enable_dns_support      = true
+    enable_dns_hostnames    = true
+    
+    tags = {
+        Name = "rabbit-main-public"
+    }
+}
+
+resource "aws_internet_gateway" "rabbit-gateway" {
+    vpc_id = aws_vpc.rabbit-vpc-public.id
+}
+
+resource "aws_subnet" "rabbit-vpc-public" {
+    vpc_id              = aws_vpc.rabbit-vpc-public.id
+    cidr_block          = "10.0.1.0/24"
+    availability_zone   = "ap-northeast-1a"
+
+    tags = {
+        Name = "rabbit-main-public"
+    }
+}
+
+resource "aws_route_table" "rabbit-route-table" {
+    vpc_id = aws_vpc.rabbit-vpc-public.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.rabbit-gateway.id
+    }
+}
+
+resource "aws_route_table_association" "rabbit-association" {
+    subnet_id       = aws_subnet.rabbit-vpc-public.id
+    route_table_id  = aws_route_table.rabbit-route-table.id
+}
+
+data "aws_ami" "ubuntu" { 
     most_recent = true
 
     filter {
@@ -25,8 +63,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "rabbit-env-server" {
-    ami           = data.aws_ami.ubuntu.id
-    instance_type = "t3.micro"
+    ami                         = data.aws_ami.ubuntu.id
+    instance_type               = "t3.micro"
+    associate_public_ip_address = true
+    key_name                    = "greg-rabbit-key"
+    vpc_security_group_ids      = ["sg-001f767ddb67db14d"]
 
     tags = {
         Name = "rabbit-env-server"
