62d55d57503e62088cca59b664b293640fbcc116
jom-hedges <jom_hedges@proton.me>
2025-10-30 17:18:45 +0800

    found a typo, :)

--- apps/useless-box/src/index.ts
+++ apps/useless-box/src/index.ts
@@ -3,8 +3,9 @@ import { toggleSwitch, getState } from "./state";
 
 const app =new Elysia()
 
-app.get('/state', () => getState());
-app.post('/toggle' () => toggleSwitch());
+app
+    .get('/state', () => getState())
+    .post('/toggle', () => toggleSwitch());
 
 app.listen(3000)
 console.log('Useless machine running on http://localhost:3000')
