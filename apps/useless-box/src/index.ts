ddd640f2a5f575b32fcded91b2b6335c65c947ed
jom-hedges <jom_hedges@proton.me>
2025-11-03 20:50:52 +0800

    refactored bc i learned about state

--- apps/useless-box/src/index.ts
+++ apps/useless-box/src/index.ts
@@ -1,15 +1,38 @@
 import { Elysia } from "elysia";
-import { toggleSwitch, getState } from "./state";
-import { startStateWatcher } from "./loop";
 
 const app =new Elysia()
+  .state("alive", false)
+  .get("/", ({ store }) => ({
+    // expose current value so it's visible in the browser
+    alive: store.alive
+  }))
+  .post("/on", ({ store }) => {
+    // any request can turn the switch on
+    store.alive = true;
+    return { message: "SWITCH turned ON"};
+  })
+  .listen(3000);
 
-app
-    .get('/state', () => getState())
-    .post('/toggle', () => toggleSwitch());
+// start a single_interval that watches the flag
+const INTERVAL_MS = 1_000;
+
+const watcher = setInterval(() = > {
+  // `app.store`  gives us the shared store object
+  if (app.store.alive) {
+    console.log("SWITCH is ON -> switching OFF");
+    app.store.alive = false; // flips it back OFF
+  } else {
+    console.log("SWITCH is already OFF");
+  }
+}, INTERVAL_MS);
+
+// cleans up on process exit 
+process.on("SIGINT", () => {
+  clearInterval(watcher);
+  app.stop();
+  console.log("慢走")；
+  process.exit(0);
+});
 
 
 
-app.listen(3000)
-console.log('Useless machine running on http://localhost:3000')
-startStateWatcher();
