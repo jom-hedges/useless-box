359b255c95a87eba38944efdc82a0732f7a34b74
jom-hedges <jom_hedges@proton.me>
2025-11-06 17:15:53 +0800

    added endpoints for onStart, toggle, and state

--- apps/useless-box/src/index.ts
+++ apps/useless-box/src/index.ts
@@ -1,19 +1,72 @@
 import { Elysia } from "elysia";
-import { 
-    PutItemCommand,
-    ScanCommand, 
-    GetItemCommand,
-    DynamoDBClient,
-} from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    GetCommand,
+    PutCommand,
+    DynamoDBDocumentClient, 
+} from "@aws-sdk/lib-dynamodb";
 
-const TABLE_NAME = "UselessBoxTable";
-const REGION = "ap-northheast-1";
+// one client for the whole process
+const ddb = new DynamoDBClient({ region: "ap-northeast-1", });
+const ddbDocClient = DynamoDBDocumentClient.from(ddb);
 
-const dbClient = new DynamoDBClient({ region: REGION });
+const TABLE_NAME = "useless-box";
+const PK = "useless-box-001";
 
 const app = new Elysia()
+    .state('ddb', ddb)
+    
+    // ensure a record exists (run once at startup)
+    .onStart(async ({ ddb }) => {
+        const { Item } await state.ddb.send(
+            new GetCommand({ 
+                TableName: "useless-box", 
+                Item: { pk: "useless-box-001", on: false }
+            })
+            if (!Item) {
+                await ddb.send(
+                    new PutCommand({
+                        TableName: TABLE_NAME,
+                        Item: { pk: PK, on: false }
+                    })
+                );
+            }
+        })
+    
+    // get the current state
+    .get("/state", async ({ ddb }) => {
+        const { Item } = await ddb.send(
+            new GetCommand({
+                TableName: TABLE_NAME,
+                Key: { pk: PK }
+            })
+        );
 
+        return Item;
+    })
 
+    // toggle the state
+    .post("/toggle", async ({ ddb }) => {
+        const { Item } = await ddb.send(
+            new GetCommand({
+                TableName: TABLE_NAME,
+                Key: { pk: PK }
+            })
+        );
 
+        const newState = !Item.on;
 
+        await ddb.send(
+            new PutCommand({
+                TableName: TABLE_NAME,
+                Item: { pk: PK, on: newState }
+            })
+        );
 
+        return { pk: PK, on: newState };
+    });
+
+app.listen(3000, () => console.log("Useless Box app is running on port 3000"));
+        
+    
+  
